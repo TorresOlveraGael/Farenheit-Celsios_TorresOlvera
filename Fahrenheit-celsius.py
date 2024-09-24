79e7c6b8c5cb098a8e4b150d15646a37d78bba92
TorresOlveraGael <gaeltorreso23.cb128@dgeti.sems.gob.mx>
2024-09-23 20:33:34 -0600

    Add files via upload

---
+++ Fahrenheit-celsius.py
@@ -0,0 +1,11 @@
+#En esta actividad, realizaremos un codigo que realiza una conversion de grados farenheit a grados celsius
+
+print (" ") 
+print ("Torres Olvera Gael") 
+print (" ")
+
+fahrenheit = int(input("Introduce los grados fahrenheit: ")) #Solicita al usuario introducir un valor
+
+Celsius = (5/9) * (fahrenheit-32) #Indica al programa para realizar la conversion
+
+print ("La conversion es de: ", Celsius) #Muestra el resultado
